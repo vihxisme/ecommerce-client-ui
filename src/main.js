e74b1fd5c9b84ffec2210c74d0d0df23e0d5879d
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-12 23:04:45 +0700

    project configuration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,56 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Vue Router
+import { createRouter, createWebHistory } from "vue-router";
+import routes from "@/routes/routes";
+
+// Vuetify
+import "vuetify/styles"; // Vuetify styles
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// tiktok logo
+import tiktoklogo from '@/assets/images/documentation/tiktok-logo.svg';
+
+// Material Design Icons (MDI)
+import "@mdi/font/css/materialdesignicons.css";
+
+// SCSS Styles
+import "@/styles/index.scss";
+
+const customIcons = {
+  tiktok: {
+    component: tiktoklogo, // Định nghĩa component SVG
+  },
+};
+
+// Khởi tạo Vue Router
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Khởi tạo Vuetify
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',  // Sử dụng Material Design Icons
+    aliases,
+    sets: {
+      mdi,
+      custom: customIcons,
+    },
+  },
+  components,
+  directives,
+})
+
+// Khởi tạo ứng dụng Vue
+const app = createApp(App);
+
+app.use(router); // Đăng ký Vue Router
+app.use(vuetify); // Đăng ký Vuetify
+
+app.mount("#app"); // Mount ứng dụng vào phần tử #app
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // Alias mặc định
+        'components': path.resolve(__dirname, 'src/components'), // Alias cho components
+        'assets': path.resolve(__dirname, 'src/assets'), // Alias cho assets
+      },
+    },
+  },
+};
