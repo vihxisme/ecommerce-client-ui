31a39a32234e28c5a505b79ee10c3e872b253c5c
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-20 00:26:53 +0700

    upd: route - shirt... Product

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,14 @@ import GeneralLayout from "@/layouts/GeneralLayout.vue";
 import HomePage from "@/pages/Home/HomePage.vue";
 import NewProduct from "@/pages/Products/NewProduct.vue";
 import PromoProduct from "@/pages/Products/PromoProduct.vue";
+import ShirtProduct from "@/pages/Products/ShirtProduct.vue";
 import TshirtProduct from "@/pages/Products/TshirtProduct.vue";
+import FormalShirtProduct from "@/pages/Products/FormalShirtProduct.vue";
+import PoloShirtProduct from "@/pages/Products/PoloShirtProduct.vue";
+import JacketProduct from "@/pages/Products/JacketProduct.vue";
+import SweaterProduct from "@/pages/Products/SweaterProduct.vue";
+import WoolSweaterProduct from "@/pages/Products/WoolSweaterProduct.vue";
+import BlazerProduct from "@/pages/Products/BlazerProduct.vue";
 
 const routes = [
   {
@@ -26,9 +33,44 @@ const routes = [
         component: PromoProduct
       },
       {
+        path: "products/shirt",
+        name: "Shirt",
+        component: ShirtProduct
+      },
+      {
         path: "products/shirt/t-shirt",
-        name: "T-shirt Product",
+        name: "T-shirt",
         component: TshirtProduct
+      },
+      {
+        path: "products/shirt/formal-shirt",
+        name: "Formal-Shirt",
+        component: FormalShirtProduct
+      },
+      {
+        path: "products/shirt/polo-shirt",
+        name: "Polo-Shirt",
+        component: PoloShirtProduct
+      },
+      {
+        path: "products/shirt/jacket",
+        name: "Jacket",
+        component: JacketProduct
+      },
+      {
+        path: "products/shirt/sweater",
+        name: "Sweater",
+        component: SweaterProduct,
+      },
+      {
+        path: "products/shirt/wool-sweater",
+        name: "Wool Sweater",
+        component: WoolSweaterProduct
+      },
+      {
+        path: "products/shirt/blazer",
+        name: "Blazer",
+        component: BlazerProduct
       }
     ],
   },
