a4113eb8cf8f88a1a90a84fb4d29ddd1c5efbf94
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-19 23:38:32 +0700

    register PromoProduct with routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import GeneralLayout from "@/layouts/GeneralLayout.vue";
 import HomePage from "@/pages/Home/HomePage.vue";
 import NewProduct from "@/pages/Products/NewProduct.vue";
+import PromoProduct from "@/pages/Products/PromoProduct.vue";
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
         path: "products/new",
         name: "New Product",
         component: NewProduct,
+      },
+      {
+        path: "products/promo",
+        name: "Promo Product",
+        component: PromoProduct
       }
     ],
   },
