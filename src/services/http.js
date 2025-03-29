720583e5ba2dcc2da19d2532c802032207b496d9
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-29 21:27:36 +0700

    add: http.js - configue request and response for apis

---
+++ src/services/http.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const publicEndpoints = [
+  "/public-api-1",
+  "/public-api-2",
+  "/another-public-endpoint",
+];
+
+
+const http = axios.create({
+  baseURL: "/api",
+  timeout: 5000,
+});
+
+http.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  // Kiểm tra nếu endpoint thuộc danh sách public
+  const isPublicEndpoint = publicEndpoints.some((endpoint) =>
+    config.url.includes(endpoint)
+  );
+
+  if (!isPublicEndpoint && token) {
+    // Gắn Authorization header nếu không phải endpoint public
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+
+http.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error.response);
+    return Promise.reject(error);
+  }
+);
+
+export default http;
