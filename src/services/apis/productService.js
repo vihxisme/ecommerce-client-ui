4c49fc31adf8ebf93d216fe5788779b04294f0e1
vihxisme <nguyenvanvinh9203@gmail.com>
2025-04-01 20:50:05 +0700

    build some API to get data for product

---
+++ src/services/apis/productService.js
@@ -0,0 +1,56 @@
+import http from "../http";
+
+export const getPromoProduct = (request) => {
+  return http.get("/product/v1/products/only-discount", {
+    params: {
+      page: request.page,
+      size: request.size,
+    }
+  });
+}
+
+export const getAllProduct = (request) => {
+  return http.get("v1/products/info/all", {
+    params: {
+      page: request.page,
+      size: request.size
+    }
+  });
+}
+
+export const getNewProducts = (request) => {
+  return http.get("/product/v1/products/new", {
+    params: {
+      page: request.page,
+      size: request.size
+    }
+  });
+}
+
+export const getProdByCategory = (categorieId, requestPage) => {
+  return http.get("/product/v1/products/by-categorie", {
+    params: {
+      categorieId: categorieId,
+      page: requestPage.page,
+      size: requestPage.size
+    }
+  });
+}
+
+export const getProdByCateApparelType = (apparelType, request) => {
+  return http.get("/product/v1/products/apparel-type", {
+    params: {
+      apparelType: apparelType,
+      page: request.page,
+      size: request.size
+    }
+  });
+}
+
+export const getProductDetails = (id) => {
+  return http.get("/product/v1/products/detail-info", {
+    params: {
+      id: id
+    }
+  });
+}
